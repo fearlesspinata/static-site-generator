b19e769ef6f79df787be9417170bafe4be2d2e69
fearlesspinata <vesnalay@gmail.com>
2024-08-11 20:25:43 -0700

    Adding base architecture

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from textnode import TextNode
+
+def main():
+    test1 = TextNode("a sentence", "bold", "http://www.something.com")
+
+    print(test1)
+
+if __name__ == "__main__":
+    main()
