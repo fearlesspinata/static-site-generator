8675d1847d6d961f665b150a9a30eddcbb3d4111
fearlesspinata <vesnalay@gmail.com>
2024-08-25 16:22:32 -0700

    Adding more tests

--- tests/test_parentnode.py
+++ tests/test_parentnode.py
@@ -19,3 +19,62 @@ class TestParent(unittest.TestCase):
                 ],
             )
         self.assertEqual(node1.to_html(), "<p><i>child1</i><b>child2</b>child3<p><i>child1 of child4</i>child2 of child4<b>child3 of child4</b></p></p>")
+
+    def test_none_tag(self):
+        node1 = ParentNode(
+                None,
+                [
+                    LeafNode("i", "italics"),
+                    LeafNode("b", "bold"),
+                    LeafNode(None, "Normal"),
+                    LeafNode("b", "bold"),
+                ]
+            )
+
+        with self.assertRaises(ValueError):
+            node1.to_html()
+
+    def test_multiple_parent(self):
+        node1 = ParentNode(
+                "p",
+                [
+                    ParentNode("p",
+                               [
+                                   LeafNode("i", "italics"),
+                                ]
+                               ),
+                    ParentNode("p",
+                               [
+                                   LeafNode("b", "bold"),
+                               ]
+                               ),
+                    ParentNode("p",
+                               [
+                                   LeafNode(None, "Normal"),
+                               ]
+                               ),
+                ],
+            )
+
+        self.assertEqual(node1.to_html(), "<p><p><i>italics</i></p><p><b>bold</b></p><p>Normal</p></p>")
+
+    def test_leaf_child(self):
+        node1 = ParentNode(
+                "p",
+                [
+                    LeafNode("i", "italics"),
+                    LeafNode("b", "bold"),
+                    LeafNode(None, "normal"),
+                    LeafNode("li", "ordered list"),
+                    LeafNode("ul", "unordered list"),
+                ]
+                )
+        self.assertEqual(node1.to_html(), "<p><i>italics</i><b>bold</b>normal<li>ordered list</li><ul>unordered list</ul></p>")
+
+    def test_no_children(self):
+        node1 = ParentNode("p", 'no children')
+        with self.assertRaises(ValueError):
+            node1.to_html()
+
+
+
